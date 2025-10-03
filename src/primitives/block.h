68cf67838c959f8a24a30204c5825cb4153b8371
Ishan Singh <isingh.bigs@gmail.com>
2025-10-02 23:35:17 -0400

    Update block.h

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -1,159 +1,60 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2017 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#ifndef BLOCK_H
+#define BLOCK_H
 
-#ifndef BITCOIN_PRIMITIVES_BLOCK_H
-#define BITCOIN_PRIMITIVES_BLOCK_H
-
-#include <primitives/transaction.h>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <stdint.h>
 #include <serialize.h>
-#include <uint256.h>
+#include <hash.h>
+#include <primitives/transaction.h>
 
-#include "crypto/scrypt.h"
-
-/** Nodes collect new transactions into a block, hash them into a hash tree,
- * and scan through nonce values to make the block's hash satisfy proof-of-work
- * requirements.  When they solve the proof-of-work, they broadcast the block
- * to everyone and the block is added to the block chain.  The first transaction
- * in the block is a special one that creates a new coin owned by the creator
- * of the block.
- */
-class CBlockHeader
-{
+class CBlockHeader {
 public:
-    // header
-    int32_t nVersion;
+    // Existing fields for block header
+    int nVersion;
     uint256 hashPrevBlock;
     uint256 hashMerkleRoot;
     uint32_t nTime;
     uint32_t nBits;
     uint32_t nNonce;
 
-    CBlockHeader()
-    {
-        SetNull();
-    }
+    // New: QC (quorum certificate) field for PoA
+    std::vector<unsigned char> qc;
 
-    ADD_SERIALIZE_METHODS;
+    // Helper method: Get value hash (signers sign this)
+    uint256 GetValueHash() const;
 
+    // Serialization method for block header
     template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream& s, Operation ser_action) {
-        READWRITE(this->nVersion);
+    void Serialize(Stream& s, Operation ser_action) {
+        READWRITE(nVersion);
         READWRITE(hashPrevBlock);
         READWRITE(hashMerkleRoot);
         READWRITE(nTime);
         READWRITE(nBits);
         READWRITE(nNonce);
+        // Serialize qc (quorum certificate)
+        READWRITE(qc);
     }
 
-    void SetNull()
-    {
-        nVersion = 0;
-        hashPrevBlock.SetNull();
-        hashMerkleRoot.SetNull();
-        nTime = 0;
-        nBits = 0;
-        nNonce = 0;
-    }
-
-    bool IsNull() const
-    {
-        return (nBits == 0);
-    }
-
-    uint256 GetHash() const;
-
-    uint256 GetPoWHash(int nAlgo) const;
-
-    int64_t GetBlockTime() const
-    {
-        return (int64_t)nTime;
+    // Compute block hash (excluding qc)
+    uint256 GetHash() const {
+        CHashWriter ss(SER_GETHASH, PROTOCOL_VERSION);
+        ss << nVersion;
+        ss << hashPrevBlock;
+        ss << hashMerkleRoot;
+        ss << nTime;
+        ss << nBits;
+        ss << nNonce;
+        return ss.GetHash();
     }
 };
 
-
-class CBlock : public CBlockHeader
-{
+class CBlock : public CBlockHeader {
 public:
-    // network and disk
-    std::vector<CTransactionRef> vtx;
+    std::vector<CTransactionRef> vtx;  // Transactions in the block
 
-    // memory only
-    mutable bool fChecked;
-
-    CBlock()
-    {
-        SetNull();
-    }
-
-    CBlock(const CBlockHeader &header)
-    {
-        SetNull();
-        *((CBlockHeader*)this) = header;
-    }
-
-    ADD_SERIALIZE_METHODS;
-
+    // Serialization for the entire block (including transactions)
     template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream& s, Operation ser_action) {
-        READWRITE(*(CBlockHeader*)this);
-        READWRITE(vtx);
-    }
-
-    void SetNull()
-    {
-        CBlockHeader::SetNull();
-        vtx.clear();
-        fChecked = false;
-    }
-
-    CBlockHeader GetBlockHeader() const
-    {
-        CBlockHeader block;
-        block.nVersion       = nVersion;
-        block.hashPrevBlock  = hashPrevBlock;
-        block.hashMerkleRoot = hashMerkleRoot;
-        block.nTime          = nTime;
-        block.nBits          = nBits;
-        block.nNonce         = nNonce;
-        return block;
-    }
-
-    std::string ToString() const;
-};
-
-/** Describes a place in the block chain to another node such that if the
- * other node doesn't have the same branch, it can find a recent common trunk.
- * The further back it is, the further before the fork it may be.
- */
-struct CBlockLocator
-{
-    std::vector<uint256> vHave;
-
-    CBlockLocator() {}
-
-    explicit CBlockLocator(const std::vector<uint256>& vHaveIn) : vHave(vHaveIn) {}
-
-    ADD_SERIALIZE_METHODS;
-
-    template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream& s, Operation ser_action) {
-        int nVersion = s.GetVersion();
-        if (!(s.GetType() & SER_GETHASH))
-            READWRITE(nVersion);
-        READWRITE(vHave);
-    }
-
-    void SetNull()
-    {
-        vHave.clear();
-    }
-
-    bool IsNull() const
-    {
-        return vHave.empty();
-    }
-};
-
-#endif // BITCOIN_PRIMITIVES_BLOCK_H
+    void Serialize(Stream& s, Operation ser_action)
